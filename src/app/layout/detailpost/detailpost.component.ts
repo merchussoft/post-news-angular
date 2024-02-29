ea081a2c19517dd1ff5756706eec6997c757df8a
MERCHUS-SERVER\merch <merchussoft@hotmail.com>
2024-02-29 17:45:10 -0500

    se hace ajuste para que las imagenes siempre tengan el mismo tamaño y que el tamaño no afecte el diseño de las paginas ademas tambien se le ajusta unas funciones meta

--- src/app/layout/detailpost/detailpost.component.ts
+++ src/app/layout/detailpost/detailpost.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {GamersoftnewsService} from "../../services/gamersoftnews.service";
 import {NgxSpinnerService} from "ngx-spinner";
+import { Meta, Title  } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-detailpost',
@@ -16,7 +17,9 @@ export class DetailpostComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private http: GamersoftnewsService,
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    private meta: Meta,
+    private titleService: Title
   ) {
     this.cod_news = this.route.snapshot.paramMap.get('cod_news');
   }
@@ -26,6 +29,11 @@ export class DetailpostComponent implements OnInit {
     this.http.traerPost(Number(this.cod_news))
       .subscribe(response => {
         this.spinner.hide();
+        console.log(response[0]);
+        let data_response = response[0]
+        this.meta.updateTag({ property: 'og:image', content: data_response.url_img});
+        this.meta.updateTag({ property: 'og:title', content: data_response.title});
+        this.titleService.setTitle(data_response.title);
         this.data_post_one = response;
       })
   }
